2620ca05a18da94ff81f37ba5d0f4b9af5e41d62
Stefan Iordache <iordachs@gmail.com>
2024-05-30 09:24:07 +0300

    feat: companies page

--- src/app/aisaac-pages/administration/companies/tabs/companies-users/companies-users.component.ts
+++ src/app/aisaac-pages/administration/companies/tabs/companies-users/companies-users.component.ts
@@ -1,18 +1,63 @@
+import { SelectionModel } from '@angular/cdk/collections';
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faMagnifyingGlass, faXmark } from '@fortawesome/free-solid-svg-icons';
-import {} from '@ng-bootstrap/ng-bootstrap';
+import {
+  faEllipsisVertical,
+  faInfo,
+  faMagnifyingGlass,
+  faXmark,
+} from '@fortawesome/free-solid-svg-icons';
+import { NgbDropdownModule, NgbPopover } from '@ng-bootstrap/ng-bootstrap';
 import { AddNewDrpComponent } from './add-new-drp/add-new-drp.component';
+import { dataUsers } from './companies-users.usersdata';
 import { ContextDrpComponent } from './context-drp/context-drp.component';
-
+export interface usersDataRow {
+  id: number;
+  name: string;
+  email: string;
+  role: string;
+  company: string;
+  buAccess: string;
+  status: string;
+  mobileNr: string;
+  factorAuth: boolean;
+  emailMessage: boolean;
+  smsMessage: boolean;
+  createdBy: string;
+  createdDate: string;
+  updateBy: string;
+  updatedDate: string;
+}
 @Component({
   selector: 'app-companies-users',
   standalone: true,
-  imports: [FontAwesomeModule, AddNewDrpComponent, ContextDrpComponent],
+  imports: [
+    FontAwesomeModule,
+    AddNewDrpComponent,
+    ContextDrpComponent,
+    NgbPopover,
+    NgbDropdownModule,
+  ],
   templateUrl: './companies-users.component.html',
   styleUrl: './companies-users.component.scss',
 })
 export class CompaniesUsersComponent {
   faXmark = faXmark;
   faMagnifyingGlass = faMagnifyingGlass;
+  faInfo = faInfo;
+  faEllipsisVertical = faEllipsisVertical;
+  usersData = dataUsers.data;
+  usersCount = dataUsers.count;
+  selection = new SelectionModel<usersDataRow>(true, []);
+
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.usersData.length;
+    return numSelected === numRows;
+  }
+  masterToggle() {
+    this.isAllSelected()
+      ? this.selection.clear()
+      : this.usersData.forEach((row) => this.selection.select(row));
+  }
 }
--- src/app/aisaac-pages/administration/companies/tree-sidebar/tree-sidebar.component.ts
+++ src/app/aisaac-pages/administration/companies/tree-sidebar/tree-sidebar.component.ts
@@ -1,15 +1,16 @@
-import { Component } from '@angular/core';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
+import { NestedTreeControl } from '@angular/cdk/tree';
 import { CommonModule } from '@angular/common';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
-import {MatTreeModule, MatTreeNestedDataSource} from '@angular/material/tree';
-import {NestedTreeControl} from '@angular/cdk/tree';
-import {MatCardModule} from '@angular/material/card';
-import {MatDialog} from '@angular/material/dialog';
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDialog } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatTreeModule, MatTreeNestedDataSource } from '@angular/material/tree';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCaretDown, faCaretRight } from '@fortawesome/free-solid-svg-icons';
 import { AddEditNewCompanyComponent } from './add-edit-new-company/add-edit-new-company.component';
-
 
 interface companyNode {
   name: string;
@@ -18,48 +19,58 @@ interface companyNode {
 }
 
 const TREE_DATA: companyNode[] = [
-
   {
     name: 'Eviden',
-    id:'15',
+    id: '15',
     children: [
       {
         name: 'Comcast',
-        id:'16',
-        children: [{
-          name: 'Dunkin Donuts',
-          id : '1',
-          children: [{id:'13',name: 'Dunkin Franchise'}, {id:'14',name: 'Some Company Name'}],
-        },
-        {
-          id:'2',
-          name: 'Some Company Name',
-        },],
+        id: '16',
+        children: [
+          {
+            name: 'Dunkin Donuts',
+            id: '1',
+            children: [
+              { id: '13', name: 'Dunkin Franchise' },
+              { id: '14', name: 'Some Company Name' },
+            ],
+          },
+          {
+            id: '2',
+            name: 'Some Company Name',
+          },
+        ],
       },
       {
         name: 'HDFC',
-        id:'3',
-        children: [{
-          name: 'Some Company Name',
-          id:'4',
-          children: [{id:'5',name: 'Some Company Name'}, {id:'6',name: 'Some Company Name'},{id:'7',name: 'Some Company Name'}, {id:'8',name: 'Some Company Name'}],
-        },
-        {
-          id:'9',
-          name: 'Some Company Name',
-        },
-        {
-          id:'10',
-          name: 'Some Company Name',
-        },
-      ],
+        id: '3',
+        children: [
+          {
+            name: 'Some Company Name',
+            id: '4',
+            children: [
+              { id: '5', name: 'Some Company Name' },
+              { id: '6', name: 'Some Company Name' },
+              { id: '7', name: 'Some Company Name' },
+              { id: '8', name: 'Some Company Name' },
+            ],
+          },
+          {
+            id: '9',
+            name: 'Some Company Name',
+          },
+          {
+            id: '10',
+            name: 'Some Company Name',
+          },
+        ],
       },
       {
-        id:'11',
+        id: '11',
         name: 'Yes Bank',
       },
       {
-        id:'12',
+        id: '12',
         name: 'Axis Bank',
       },
     ],
@@ -69,48 +80,56 @@ const TREE_DATA: companyNode[] = [
 @Component({
   selector: 'app-tree-sidebar',
   standalone: true,
-  imports: [MatCheckboxModule,MatIconModule,MatButtonModule,
-    CommonModule,MatProgressBarModule,MatTreeModule,MatCardModule,AddEditNewCompanyComponent],
+  imports: [
+    MatCheckboxModule,
+    MatIconModule,
+    MatButtonModule,
+    CommonModule,
+    MatProgressBarModule,
+    MatTreeModule,
+    MatCardModule,
+    AddEditNewCompanyComponent,
+    FontAwesomeModule,
+  ],
   templateUrl: './tree-sidebar.component.html',
-  styleUrl: './tree-sidebar.component.scss'
+  styleUrl: './tree-sidebar.component.scss',
 })
 export class TreeSidebarComponent {
-hover(input: string,id:any) {
-  if(input === 'in'){
-    console.log(id);
-
+  faCaretRight = faCaretRight;
+  faCaretDown = faCaretDown;
+  hover(input: string, id: any) {
+    if (input === 'in') {
+      console.log(id);
+    } else {
+      console.log(id);
+    }
   }
-  else{
-    console.log(id);
+  hoverOnComp(input: string, id: any) {
+    const element = document.getElementById(id);
+    if (input === 'in') {
+      id = true;
+    } else {
+      if (element) {
+        id = false;
+      }
+    }
   }
-}
-hoverOnComp(input: string,id:any) {
-  const element = document.getElementById(id);
-  if(input === 'in'){
-    id = true;
+  handleClick() {
+    console.log('Icon clicked!');
   }
-  else{
-  if (element) {
-    id = false;
-  }
-  }
-}
-handleClick() {
-  console.log('Icon clicked!');
-}
-  treeControl = new NestedTreeControl<companyNode>(node => node.children);
+  treeControl = new NestedTreeControl<companyNode>((node) => node.children);
   dataSource = new MatTreeNestedDataSource<companyNode>();
-  isHovered: boolean = false;
-  isHoveredOnComp: boolean = false;
+  isHovered = false;
+  isHoveredOnComp = false;
   constructor(public dialog: MatDialog) {
     this.dataSource.data = TREE_DATA;
   }
-  hasChild = (_: number, node: companyNode) => !!node.children && node.children.length > 0;
-  openDialog(){
-    this.dialog.open(AddEditNewCompanyComponent,{
+  hasChild = (_: number, node: companyNode) =>
+    !!node.children && node.children.length > 0;
+  openDialog() {
+    this.dialog.open(AddEditNewCompanyComponent, {
       width: '700px',
-      data: {}
+      data: {},
     });
   }
 }
-
